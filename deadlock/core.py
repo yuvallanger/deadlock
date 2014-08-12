71336b6a59e8868481dd3b9f26c820009ef59cfc
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2014-08-12 14:59:03 +0100

    Main 3 commands now working in script mode.

--- deadlock/core.py
+++ deadlock/core.py
@@ -5,8 +5,14 @@ import zipfile
 import tempfile
 import getpass
 import argparse
+import warnings
 
-from . import crypto
+# PyNaCl issues a warning every time it's imported:
+#   UserWarning: reimporting '_cffi__x873aa75dx8ee09fd4' might overwrite older definitions
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from . import crypto
+
 from .passwords import zxcvbn, phrase
 
 __all__ = [ 'Settings',
@@ -22,8 +28,12 @@ class Settings:
     minKeyEntropy = 100
 
 def check_passphrase(pp, email):
-    user, domain = email.split("@")
-    domain = domain.rsplit(".", 1)[0]
+    try:
+        user, domain = email.split("@")
+        domain = domain.rsplit(".", 1)[0]
+    except:  # Not a valid email address, who cares jeeze
+        user = email
+        domain = email
     score = zxcvbn.password_strength(pp, user_inputs=[user, domain])
     if score['entropy'] >= Settings.minKeyEntropy:
         return True, score
@@ -44,7 +54,8 @@ def make_lock_securely():
         if ok: break
         print("Insufficiently strong passphrase; has {entropy} bits of entropy, could be broken in {crack_time_display}".format(**score))
         print("Suggestion:", make_random_phrase(email))
-    key = crypto.UserKey.from_passphrase(email, passphrase)
+    key = crypto.UserLock.from_passphrase(email, passphrase)
+    return key
     
 def encrypt_file(file_path, sender, recipients):
     "Returns encrypted binary file content if successful"
@@ -97,45 +108,51 @@ def error_out(message, code=1):
     sys.exit(code)
 
 def main_encrypt(A):
-    A.userKey = make_lock_securely()
+    userKey = make_lock_securely()
+    print("User ID:", userKey.userID)
     if not os.path.exists(A.path):
         error_out("File or directory '{}' does not exist.".format(A.path))
     if os.path.isfile(A.path):
-        crypted = encrypt_file(A.path, A.userKey, [A.userKey, A.recipients])
+        crypted = encrypt_file(A.path, userKey, [userKey] + A.recipient)
     elif os.path.isdir(A.path):
-        crypted = encrypt_folder(A.path, A.userKey, [A.userKey, A.recipients])
+        crypted = encrypt_folder(A.path, userKey, [userKey] + A.recipient)
     else:
         error_out("Specified path '{}' is neither a file nor a folder.".format(A.path))
     if A.base64:
         crypted = crypto.b64encode(crypted)
-    A.output.write(crypted)
+    if not A.output:
+        A.output = crypto.b64encode(os.urandom(6)) + ".minilock"
+    print("Saving output to", A.output)
+    with open(A.output, "wb") as O:
+        O.write(crypted)
 
 def main_decrypt(A):
-    A.userKey = make_lock_securely()
+    userKey = make_lock_securely()
+    print("User ID:", userKey.userID)
     if not os.path.exists(A.path):
         error_out("File or directory '{}' does not exist.".format(A.path))
     if os.path.isfile(A.path):
-        filename, decrypted = decrypt_file(A.path, A.userKey, base64 = A.base64)
+        filename, decrypted = decrypt_file(A.path, userKey, base64 = A.base64)
     else:
         error_out("Specified path '{}' is not a file.".format(A.path))
+    print("Saving output to", filename)
     with open(filename, "wb") as O:
         O.write(decrypted)
 
 def main_generate(A):
-    A.userKey = make_lock_securely()
-    print("Your miniLock ID is:", A.userLock.userID)
+    userLock = make_lock_securely()
+    print("Your miniLock ID is:", userLock.userID)
     print("Give this to anyone you want to communicate with privately to safely encrypt things to you. Remember; while your email address is used to securely generate a unique key, you can use miniLock over any medium. For output you can paste anywhere (instead of an encrypted binary file), pass the 'base64' option to deadlock in encrypt mode.")
     
 def main():
     P = argparse.ArgumentParser(description="deadlock: A stateless Python implementation of minilock.io")
-    P.set_defaults(func = lambda A: error_out("No mode specified; use either 'encrypt' or 'decrypt', or --help for information"))
     # == Create subcommands ==
     subP = P.add_subparsers(description="Modes of operation")
-    encP = subP.add_parser()
+    encP = subP.add_parser('encrypt')
     encP.set_defaults(func = main_encrypt)
-    decP = subP.add_parser()
+    decP = subP.add_parser('decrypt')
     decP.set_defaults(func = main_decrypt)
-    genP = subP.add_parser()
+    genP = subP.add_parser('generate')
     genP.set_defaults(func = main_generate)
     # == Begin args ==
     # == Encryption ==
@@ -143,8 +160,10 @@ def main():
             help="Print a block of base64 text that can be pasted anywhere. NOT compatible with miniLock for Chrome.")
     encP.add_argument("path", type=str,
             help = "Path to file to encrypt. If a folder, it will be zipped before encryption.")
-    encP.add_argument("-o", "--output", type=argparse.FileType("wb"), default=sys.stdout.buffer,
-            help = "File or stream to write output to, default is stdout.")
+    encP.add_argument("recipient", nargs="+", type=str,
+            help = "User IDs to encrypt to")
+    encP.add_argument("-o", "--output", type=str, default='',
+            help = "File to write output to, default is <8 random chars>.minilock")
     # == Decryption ==
     decP.add_argument("path", type=str,
             help = "Path to file to decrypt.")
@@ -153,4 +172,8 @@ def main():
     # == Generate ==
     # == End args; Apply args ==
     A = P.parse_args()
-    A.func(A)
+    if hasattr(A, 'func'):
+        A.func(A)
+    else:
+        error_out("No mode specified; use either 'encrypt' or 'decrypt', or --help for information")
+
