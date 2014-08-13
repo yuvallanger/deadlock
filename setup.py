12dcc9807498b35b154bee56f8712f33b27dfde1
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2014-08-13 22:59:40 +0100

    Thanks to jp.fox I know this works on 3.2, now. :)

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Environment :: Console",
         "Natural Language :: English",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4"
     ]
